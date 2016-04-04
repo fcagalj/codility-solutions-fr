093465a644ab155667c54bf4bd78f8d0bf1fc0a4
fcagalj <fcagalj@gmail.com>
2016-04-04 22:25:43 +0200

    start 2016-04

--- test/CodilityAD1Z1_Spec.js
+++ test/CodilityAD1Z1_Spec.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
-var testName = 'CodilityFR1.js';
+var testName = 'CodilityAD1Z1.js';
 var Task = require('../lib/' + testName);
-//mocha --grep CodilityFR1
+//mocha --grep CodilityAD1Z1
 describe(testName, function () {
     describe('Final results', function () {
         it('should return 4 from A = [5, 5, 1, 7, 2, 3, 5], X = 5', function () {
@@ -16,7 +16,7 @@ describe(testName, function () {
             var results = Task.solution(X, A);
             expect(results).to.equal(9);
         });
-        it('should return undefined from A = [8, 8, 8, 8, 8], X = 3', function () {
+        it('should return 9 from A = [8, 8, 8, 8, 8], X = 3', function () {
             var A = [8, 8, 8, 8, 8];
             var X = 3;
             var results = Task.solution(X, A);
