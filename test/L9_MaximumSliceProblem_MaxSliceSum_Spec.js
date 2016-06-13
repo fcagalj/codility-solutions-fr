2023137054f87e074897d71abd63ef2fb4c9bfcd
fcagalj@gmail.com <fcagalj@gmail.com>
2016-06-13 10:13:44 +0200

    max double slice

---
+++ test/L9_MaximumSliceProblem_MaxSliceSum_Spec.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+var testName = 'L9_MaximumSliceProblem_MaxSliceSum.js';
+var Task = require('../lib/' + testName);
+
+//mocha --grep MaxSliceSum
+//node-debug _mocha --grep MaxSliceSum
+describe(testName, function () {
+    describe('Final results', function () {
+        it('should return  ', function () {
+            var A = [];
+            A[0] = 3;
+            A[1] = 2;
+            A[2] = -6;
+            A[3] = 4;
+            A[4] = 0;
+            var results = Task.solution(A);
+            expect(results).to.equal(5);
+        });
+    });
+});
